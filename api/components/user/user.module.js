16cf221637f6a5c2f730df693b366771d04881b9
Max Medina <maxmedina05@gmail.com>
2017-09-16 15:50:14 -0400

    Add documentation to the API. Document User module

--- api/components/user/user.module.js
+++ api/components/user/user.module.js
@@ -2,13 +2,172 @@ const express                                 = require('express');
 const router                                  = express.Router();
 const userController                          = require('./user.controller');
 
-router.route('/')
-  .get(userController.getUsers)
-  .post(userController.addUser);
+/**
+ * @swagger
+ * definition:
+ *   User:
+ *     properties:
+ *       firstName:
+ *         type: string
+ *       lastName:
+ *         type: string
+ *       email:
+ *         type: integer
+ *       password:
+ *         type: string
 
-router.route('/:userId')
-  .get(userController.getUser)
-  .put(userController.updateUser)
-  .delete(userController.deleteUser);
+ *   GeneralResponse:
+ *     required:
+ *      - success
+ *      - message
+ *      - data
+ *     properties:
+ *       success:
+ *         type: string
+ *       message:
+ *         type: string
+ *       data:
+ *         type: object
+
+ *   ErrorResponse:
+ *     required:
+ *      - success
+ *      - message
+ *     properties:
+ *       success:
+ *         type: string
+ *       message:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     tags:
+ *       - Users
+ *     description: Returns a list of users
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of users
+ *         schema:
+ *           $ref: '#/definitions/GeneralResponse'
+ *       default:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
+ */
+router.route('/users').get(userController.getUsers);
+
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     tags:
+ *       - Users
+ *     description: add a new user
+ *     parameters:
+ *        name: user
+ *        description: User properties
+ *        in: body
+ *        required: true
+ *        schema:
+ *         $ref: '#/definitions/User'
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of users
+ *         schema:
+ *           $ref: '#/definitions/GeneralResponse'
+ *       default:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
+ */
+router.route('/users').post(userController.addUser);
+
+/**
+ * @swagger
+ * /api/users/{userId}:
+ *   get:
+ *     tags:
+ *       - Users
+ *     description: Returns a specific user based on ObjectId
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *      - name: userId
+ *        description: The ObjectId of the user
+ *        in: path
+ *        type: string
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: An array of users
+ *         schema:
+ *           $ref: '#/definitions/GeneralResponse'
+ *       default:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
+ */
+router.route('/users/:userId').get(userController.getUser);
+
+/**
+ * @swagger
+ * /api/users/{userId}:
+ *   put:
+ *     tags:
+ *       - Users
+ *     description: Updates a specific user based on ObjectId
+ *     produces: application/json
+ *     parameters:
+ *      - name: userId
+ *        description: The ObjectId of the user
+ *        in: path
+ *        type: string
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: The updated user
+ *         schema:
+ *           $ref: '#/definitions/GeneralResponse'
+ *       default:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
+ */
+router.route('/users/:userId').put(userController.updateUser);
+
+/**
+ * @swagger
+ * /api/users/{userId}:
+ *   delete:
+ *     tags:
+ *       - Users
+ *     description: Deletes a specific user based on ObjectId
+ *     produces: application/json
+ *     parameters:
+ *      - name: userId
+ *        description: The ObjectId of the user
+ *        in: path
+ *        type: string
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: The user that was deleted
+ *         schema:
+ *           $ref: '#/definitions/GeneralResponse'
+ *       default:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
+ */
+router.route('/users/:userId').delete(userController.deleteUser);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,58 @@
 require('dotenv').config();
-const express                   = require('express');
-const bodyParser                = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const swaggerJSDoc = require('swagger-jsdoc');
 
-const PORT                      = process.env.PORT || 5099;
-const mongoose                  = require('mongoose');
+const PORT = process.env.PORT || 5099;
+const mongoose = require('mongoose');
 
-const app                       = express();
-const userModule                = require('./api/components/user/user.module');
-const authModule                = require('./api/components/authentication/auth.module');
+const app = express();
+const userModule = require('./api/components/user/user.module');
+const authModule = require('./api/components/authentication/auth.module');
 
-mongoose.Promise                = Promise;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use('/v1/api', express.static(__dirname + '/public/docs'));
-
+// Database configuration
+mongoose.Promise = Promise;
 mongoose.connect(process.env.DB_CONN_STR, {
   useMongoClient: true
 });
 
+// API configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use('/v1/api', express.static(__dirname + '/public/docs'));
+
+// Swagger definition
+const swaggerDefinition = {
+  info: {
+    "description": "This is the documentation for the Pothole Detector api.",
+    "version": "0.1.0",
+    "title": "Pothole Dector API",
+    "contact": {
+      "email": "maxmedina05@gmail.com"
+    }
+  },
+  host: `localhost:${PORT}`,
+  basePath: '/'
+};
+const swaggerOptions = {
+  swaggerDefinition: swaggerDefinition,
+  apis: ['./api/components/*/*.module.js']
+};
+const swaggerSpecs = swaggerJSDoc(swaggerOptions);
+
+// API routes
 app.get('/', function(req, res) {
   res.send('Hello World');
 });
 
-app.use('/users', userModule);
+app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpecs);
+});
+
+app.use('/api', userModule);
 app.use('/auth', authModule);
 
 app.listen(PORT, function() {
