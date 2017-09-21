618ab8b023efaa5c1998f5332a96b6e5912713c5
Max Medina <maxmedina05@gmail.com>
2017-09-20 20:25:32 -0400

    refactor street defect model

--- api/components/street-defect/street-defect.model.js
+++ api/components/street-defect/street-defect.model.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const StreetDefectSchema = new mongoose.Schema({
   deviceName: String,
+  xMean: Number,
+  yMean: Number,
+  zMean: Number,
+  xStd: Number,
+  yStd: Number,
+  zStd: Number,
   latitude: Number,
   longitude: Number
 }, {
