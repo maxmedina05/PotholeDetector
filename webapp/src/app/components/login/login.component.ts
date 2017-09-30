572a506ebc7a60c42fc8dc87f347c464ad8f7d2c
Max Medina <maxmedina05@gmail.com>
2017-09-30 19:25:12 -0400

    Add Google API oauth2

--- webapp/src/app/components/login/login.component.ts
+++ webapp/src/app/components/login/login.component.ts
@@ -4,6 +4,12 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
 
+declare global {
+  interface Window {
+    onSignIn;
+  }
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,25 +17,16 @@ import 'rxjs/add/operator/map';
 })
 export class LoginComponent implements OnInit {
 
-  private clientId: string = '238180417493-rt7vceimdfi6n10do1kqmf04nle1fcjo.apps.googleusercontent.com';
-  private redirectUri: string = 'http://localhost:4200';
-  private responseType: string = 'token';
-  private scope: string = 'email';
-
-  constructor(private http: Http) { }
-
-  ngOnInit() {
-
+  constructor(private http: Http) {
   }
 
-  onLogin() {
-    console.log("onLogin");
-    this.http.get(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${this.clientId}&redirect_ui=${this.redirectUri}&response_type=${this.responseType}&scope=${this.scope}`)
-      .toPromise()
-      .then( response => {
-        let r = response.json();
-        console.log(r);
-      })
+  ngOnInit() {
+    window.onSignIn = this.onSignIn;
+  }
+
+  onSignIn(googleUser) {
+    let profile = googleUser.getBasicProfile();
+    console.log(profile);
   }
 
 }
--- webapp/src/app/components/nav/nav.component.ts
+++ webapp/src/app/components/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let gapi: any;
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -12,4 +14,11 @@ export class NavComponent implements OnInit {
   ngOnInit() {
   }
 
+  signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut()
+      .then(() => {
+        console.log('User signed out.');
+      });
+  }
 }
