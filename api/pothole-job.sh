40ccb3bcb8814a94b41f634bf8fe3db89eda9483
Max Medina <maxmedina05@gmail.com>
2017-10-11 21:35:07 -0400

    modify append to file

--- api/pothole-job.sh
+++ api/pothole-job.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
-date > /home/shiro/logs/pothole-detector.logs
-node /home/shiro/projects/pucmm/pothole-project/pothole-detector-api/api/cronjob.js > /home/shiro/logs/pothole-detector.logs
+echo "Pothole Detector Cronjob Running!" >> /home/shiro/logs/pothole-detector.logs
+date >> /home/shiro/logs/pothole-detector.logs
+node /home/shiro/projects/pucmm/pothole-project/pothole-detector-api/api/cronjob.js >> /home/shiro/logs/pothole-detector.logs
