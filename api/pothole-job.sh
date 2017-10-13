3abe42cde2b66c3534fcc91225a624248664af23
Max Medina <maxmedina05@gmail.com>
2017-10-12 21:44:30 -0400

    fix cronjob

--- api/pothole-job.sh
+++ api/pothole-job.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/bash
+#!/bin/bash
 echo "Pothole Detector Cronjob Running!" >> /home/shiro/logs/pothole-detector.logs
 date >> /home/shiro/logs/pothole-detector.logs
 node /home/shiro/projects/pucmm/pothole-project/pothole-detector-api/api/cronjob.js >> /home/shiro/logs/pothole-detector.logs
