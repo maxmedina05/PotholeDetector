1525447a33dc6660d3b8e6da18855229cf970201
Max Medina <maxmedina05@gmail.com>
2017-10-12 22:15:33 -0400

    rename pothole-detector.logs to pothole-detector.log

--- api/pothole-job.sh
+++ api/pothole-job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Pothole Detector Cronjob Running!" >> /home/shiro/logs/pothole-detector.logs
-date >> /home/shiro/logs/pothole-detector.logs
-node /home/shiro/projects/pucmm/pothole-project/pothole-detector-api/api/cronjob.js >> /home/shiro/logs/pothole-detector.logs
+node /home/shiro/projects/pucmm/pothole-project/pothole-detector-api/api/cronjob.js > /home/shiro/logs/pothole-detector.log
+echo "Pothole Detector Cronjob Running!" >> /home/shiro/logs/pothole-detector.log
+date >> /home/shiro/logs/pothole-detector.log
